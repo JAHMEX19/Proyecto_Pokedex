d8c41fbba360e8f0abef3bb8dec9a8cf157f280d
Joel Alejandro <jah190594@gmail.com>
2023-07-25 22:52:36 -0600

    FEAT: Menu generation from Script to HTML

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
-document.addEventListener("DOMContentLoaded"),function(){
+document.addEventListener("DOMContentLoaded",
+	function () {
+		var generation = [
+			"Gen-01",
+			"Gen-02",
+			"Gen-03",
+			"Gen-04",
+			"Gen-05",
+			"Gen-06",
+			"Gen-07",
+		];
+        var filter = document.getElementById("filters"); //get by Id
+        var gen = ""; 
+        for(let i=0; i < generation.length; i++){
 
-var generacion = ["Gen-01","Gen-02","Gen-03","Gen-04","Gen-05","Gen-06","Gen-07"];
+            console.log(generation[i]);
 
-};  
+            //Indexamos agregando mi clase HTML con  listado de array `` (alt +96)
+
+            gen += `{<input class="radio-gens" type="radio" id="radio" name="generation" checked>
+            <label for="for" class="label-gen"> Name </label>}`
+
+        }
+
+        filter.innerHTML=gen;
+	});
+
